1e1e989399bee003230925680944d89c789747de
asiphile <asiphilemthethethwa@gmail.com>
2024-12-05 08:40:41 +0200

    installing expo image manipulator to edit a picture, and adding image screen

---
+++ src/screens/AddImageScreen.js
@@ -0,0 +1,10 @@
+import * as ImageManipulator from 'expo-image-manipulator';
+
+const editImage = async (uri) => {
+  const editedImage = await ImageManipulator.manipulateAsync(
+    uri,
+    [{ resize: { width: 800 } }],
+    { compress: 0.7, format: ImageManipulator.SaveFormat.JPEG }
+  );
+  setImage(editedImage.uri);
+};
